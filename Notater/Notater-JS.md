317b26fd3353506d7d6b7a2e73cefc95b051cd6e
muunuo <benkforland@gmail.com>
2025-09-08 11:24:11 +0200

    Array og objekt
    En øvelse på array og objekt. Fokuserer på personer, og hvordan å grunnleggende bruke array

---
+++ Notater/Notater-JS.md
@@ -0,0 +1,63 @@
+// Aray
+// Objekt 
+
+let arrayNavn = ["Ola", "Kari", "Per"];
+
+console.log(arrayNavn);
+
+
+for(let navn of arrayNavn){// viser alle navn som liste i console
+    console.log(navn);
+}
+
+console.log(arrayNavn[1]);  // viser navn nr 1 (Kari)
+
+
+
+let person = {//objekt
+    alder: 25,
+    navn: "Bert",
+    yrke: "IKT",
+};
+
+console.log(person);
+console.log(person.alder); // consol logger kun alder
+
+
+
+let arrayPerson = [ //Aray med felere variabler
+    {
+        navn: "Emma",
+        alder: 24,
+    },
+
+    {
+        navn: "Sara",
+        alder: 18,
+    },
+
+    {
+        navn: "Markus",
+        alder: 32,
+    }
+];
+
+
+console.log(arrayPerson);
+console.log(arrayPerson[1]);
+console.log(arrayPerson[1].navn); //kun navn for person 1
+
+
+arrayPerson.push(person); //legger til infoen fra person i arrayPerson
+
+console.log("\nHer er arrayen inkludert person fra person:")
+
+
+// for(let person of arrayPerson) {
+//     console.log(arrayPerson)
+// }
+
+for (let index = 0; index < arrayPerson.length; index++) {
+    console.log(arrayPerson[index].navn);
+    
+}
---
+++ oppgave/objekter.js
@@ -0,0 +1,80 @@
+// Aray
+// Objekt 
+
+let arrayNavn = ["Ola", "Kari", "Per"];
+
+console.log(arrayNavn);
+
+
+for(let navn of arrayNavn){// viser alle navn som liste i console
+    console.log(navn);
+}
+
+console.log(arrayNavn[1]);  // viser navn nr 1 (Kari)
+
+
+
+let person = {//objekt
+    alder: 25,
+    navn: "Bert",
+    yrke: "IKT",
+};
+
+console.log(person);
+console.log(person.alder); // consol logger kun alder
+
+
+
+let arrayPerson = [ //Aray med felere variabler
+    {
+        navn: "Emma",
+        alder: 24,
+    },
+
+    {
+        navn: "Sara",
+        alder: 18,
+    },
+
+    {
+        navn: "Markus",
+        alder: 32,
+    }
+];
+
+
+console.log(arrayPerson);
+console.log(arrayPerson[1]);
+console.log(arrayPerson[1].navn); //kun navn for person 1
+
+
+arrayPerson.push(person); //legger til infoen fra person i arrayPerson
+
+
+console.log("\nHer er arrayen inkludert person fra person:")
+
+// for(let person of arrayPerson) {
+//     console.log(arrayPerson)
+// }
+
+
+for (let index = 0; index < arrayPerson.length; index++) {
+    console.log(arrayPerson[index].navn);
+    
+}
+
+console.log("\nGammlinge her -->")
+//Viser kun folk som er 25+ år gammel
+
+// for (let index = 0; index < arrayPerson.length; index++) {
+//     if (arrayPerson[index].alder >= 25) {
+//     console.log(arrayPerson[index].navn);
+// }
+// }
+
+for (let index = 0; index < arrayPerson.length; index++) {
+    if (arrayPerson[index].alder >= 25) {
+    arrayPerson.slice
+    console.log(arrayPerson[index])
+}
+}
